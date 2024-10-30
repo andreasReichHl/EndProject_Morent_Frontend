cc8c4e1d721885491af8b80017ce5ba1bca1872a
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-10-30 23:17:11 +0100

    confirmation code implemented. after confirmation user is logged in

--- src/pages/ConfirmRegistrationCode.jsx
+++ src/pages/ConfirmRegistrationCode.jsx
@@ -1,13 +1,46 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/AuthProvider";
 
 export default function () {
-  const [code, setCode] = useState("");
+  const [verifyCode, setVerifyCode] = useState("");
+  const { login } = useAuth();
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  async function confirmCodeFunction(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_BACKEND + "/api/v1/auth/unlock",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({verifyCode: Number(verifyCode)}),
+        }
+      );
+      if (!response.ok) {
+        throw new Error(
+          `Error ${response.status}: ${response.statusText}. Details: ${errorMessage}`
+        );
+      }
+
+      const data = await response.json();
+      console.log(data);
+      login(data.token);
+      navigate("/home");
+    } catch (error) {
+      console.error("Error:", error.message);
+      setError(error.message);
+    }
+  }
 
   return (
     <section className="flex justify-center items-center absolute inset-0 p-6">
       <form
-        /* onSubmit={confirmCodeFunction} */
+        onSubmit={confirmCodeFunction}
         className="bg-navBG bg-opacity-40 p-6 rounded-lg shadow-lg w-96"
       >
         <h2 className="text-2xl font-semibold mb-6 text-center">
@@ -21,7 +54,7 @@ export default function () {
             den wir Ihnen per E-Mail gesendet haben
           </p>
           <input
-            onChange={(e) => setCode(e.target.value)}
+            onChange={(e) => setVerifyCode(e.target.value)}
             type="text"
             id="code"
             className="mt-1 block w-full p-2 border border-gray-300 rounded focus:outline-none"
@@ -34,6 +67,8 @@ export default function () {
         >
           Code bestätigen
         </button>
+
+        {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
 
         <div className="text-center mb-5">
           <Link to="/resend-code" className="text-blue-500">
