ff6fdbcbb6fc58fb7e7d06bcbe3c8bb7d4c884c1
andreasReichHl <reich.andreas.hl@gmail.com>
2024-10-31 19:08:32 +0100

    implements Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,33 @@ import { useState } from "react";
 import AutoCard from "./components/AutoCard";
 import LocationNav from "./components/LocationNav";
 import LocationDate from "./components/LocationDate";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             {/* <h1 className="text-8xl pb-24 mb-96">HOME</h1> */}
-            <LocationDate />
-            <div className="flex flex-wrap gap-5 p-12 mx-auto">
-                <AutoCard />
-                <AutoCard />
-                <AutoCard />
-                <AutoCard />
-                <AutoCard />
-                <AutoCard />
+            <div className="flex flex-col sm:flex-row">
+                {/* Sidebar links */}
+                <Sidebar />
+
+                {/* Hauptinhalt rechts */}
+                <div className="flex flex-col flex-grow">
+                    {/* LocationDate Komponente oben */}
+                    <div>
+                        <LocationDate />
+                    </div>
+
+                    {/* AutoCard-Komponenten in flex-wrap Anordnung darunter */}
+                    <div className="flex flex-wrap gap-10 p-4">
+                        <AutoCard />
+                        <AutoCard />
+                        <AutoCard />
+                        <AutoCard />
+                        <AutoCard />
+                        <AutoCard />
+                    </div>
+                </div>
             </div>
         </>
     );
