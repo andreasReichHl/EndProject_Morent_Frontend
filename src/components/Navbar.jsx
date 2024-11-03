36ee96a14095daba57f0981794c6817664fbf53f
René Behrens <rene.behrens@aol.com>
2024-11-03 02:21:32 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,24 @@ export default function Navbar() {
     return (
         <div className="navbar bg-navBG bg-opacity-40">
             <div className="flex-1">
-                <a className="btn btn-ghost text-3xl text-blue-700">MORENT</a>
+                <Link to={"/home"}><img className="ml-2 w-32" src="src/assets/images/logo.svg" /></Link>
+                {/* <a className="btn btn-ghost text-3xl text-blue-700">MORENT</a> */}
             </div>
             <div className="flex-none">
+                <Link to="/admin-panel">
+                 <img
+                    src="src/assets/images/vuesax/bold/setting-2.svg"
+                    alt="settingBtn"
+                    className="  mx-4"
+                />
+                </Link>
+
                 <img
                     src="src/assets/images/vuesax/bold/vuesax/bold/notification.svg"
                     alt="notification"
                     className=" mx-4"
                 />
-
-                <img
-                    src="src/assets/images/vuesax/bold/setting-2.svg"
-                    alt="settingBtn"
-                    className="  mx-4"
-                />
-
+            
                 <div className="dropdown dropdown-end">
                     <div
                         tabIndex={0}
@@ -37,22 +40,16 @@ export default function Navbar() {
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
                     >
                         <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
+                        <Link to="/profil">Profil</Link>
                         </li>
                         <li>
-                            <a>Settings</a>
+                            <Link to="/signUp">Registrieren</Link>
                         </li>
                         <li>
-                            <Link to="/login">Log In</Link>
+                            <Link to="/login">Anmelden</Link>
                         </li>
                         <li>
-                            <Link to="/signUp">Log In</Link>
-                        </li>
-                        <li>
-                            <a>Logout</a>
+                            <a>Abmelden</a>
                         </li>
                     </ul>
                 </div>
